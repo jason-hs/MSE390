d7b07963bb30e5f062734ca91cebd5b3f4607800
lnfinion <xinfinionx@gmail.com>
2015-02-13 19:29:02 -0800

    Finished Q1 part a
    Q1.m is the working 3rd order series approximation. I had a lot of
    problems turning the symbolic answer into an expression that could be
    solved for a numerical value, but finally found that the eval() function
    turns symbolic polynomials into computable equations.
    
    Q1_error is just the same thing except with each order done separately.
    I would have like to iterate through each order in a loop instead of
    writing the entire program 4 times, but couldn't figure out how to
    automatically increase the size of my matrix from 1x1 to 1x2 to 1x3 to
    1x4 for each order

--- Group Assignment 1/Q1.m
+++ Group Assignment 1/Q1.m
@@ -4,8 +4,16 @@ clear, clc
 format long %see more decimals
 n = 0 : 3;
 syms x
-z = 25*x^3 - 6*x^2 + 7*x - 88
+z = 25*x^3 - 6*x^2 + 7*x - 88;
+%t3 = taylor(z,'Order', 3)
 d = [z diff(z) diff(z,2) diff(z,3)]
 seq = d .* x.^n ./(factorial(n)) 
-x = 3
 approx = sum(seq) 
+x = 3
+result = eval(approx)
+
+%Compute true percent relative error for each approximation
+z = eval(z)
+error = (abs(z - result)/z*100)
+%error is large, expected it to be much smaller.
+
