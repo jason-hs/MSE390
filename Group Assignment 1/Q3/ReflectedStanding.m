29080ea19c43e2bd4a144da5774b36c335d637a5
Jason Hamilton-Smith <hs.jason@gmail.com>
2015-02-17 21:59:41 -0800

    Gotta work on my greek =-)

--- Group Assignment 1/Q3/ReflectedStanding.m
+++ Group Assignment 1/Q3/ReflectedStanding.m
@@ -1,8 +1,8 @@
-function [ h ] = ReflectedStanding( h0, x, theta, t, v )
+function [ h ] = ReflectedStanding( h0, x, lambda, t, v )
 %REFLECTEDSTANDING Summary of this function goes here
 %   Detailed explanation goes here
 
-    h = h0 * ( sin( 2 * pi * x / theta ) * cos( 2 * pi * t * v / theta ) + exp( -1 * x ) );
+    h = h0 * ( sin( 2 * pi * x / lambda ) * cos( 2 * pi * t * v / lambda ) + exp( -1 * x ) );
 
 end
 
